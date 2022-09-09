0f9f8415fafc7afa64aa6a0db0e6cbe81ad67a05
路友 <1814240705@qq.com>
2022-09-09 17:00:14 +0800

    基础分页组件

---
+++ examples/views/Pagination.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <zt-pagination :total="100" :limit="10" :page="1" @pageChange="pageChange"></zt-pagination>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pagination',
+
+  data() {
+    return {}
+  },
+  methods: {
+    pageChange(page) {
+      this.$msg('第' + page + '页')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
---
+++ packages/components/Pagination/Pagination.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="page">
+    <span class="page-block center" @click="pageChange(1)">{{'«'}}</span>
+    <span v-if="pageList[2]-2>1" class="page-block center">...</span>
+    <span
+      v-for="pageNum in pageList"
+      :key="pageNum"
+      class="page-block center"
+      :style="{color:pageNum===page?'#80bd01':'#778087'}"
+      @click="pageChange(pageNum)"
+    >{{pageNum}}</span>
+    <span v-if="pageMax-pageList[2]>2" class="page-block center">...</span>
+    <span class="page-block center" @click="pageChange(pageMax)">{{'»'}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ZtPagination',
+  props: {
+    // 内容总数
+    total: {
+      type: Number,
+      default: 0
+    },
+    // 每页数量
+    limit: {
+      type: Number,
+      default: 10
+    },
+    // 当前页码
+    page: {
+      type: Number,
+      default: 1
+    }
+  },
+  data() {
+    return {
+      pageList: [] // 页码列表
+    }
+  },
+  computed: {
+    // 最大页数
+    pageMax() {
+      return Math.ceil(this.total / this.limit)
+    }
+  },
+  created() {
+    this.initData()
+  },
+  methods: {
+    initData() {
+      this.pageList = [] // 清空页码
+      var i = 1
+      do {
+        this.pageList.push(i)
+        i++
+      } while (i <= this.pageMax)
+      this.pageList.length > 5 && // 最多显示5页
+        (this.pageList = this.pageList.slice(0, 5))
+    },
+    // 子组件事件回调：分页
+    pageChange(pageCurrent) {
+      this.$emit('pageChange', pageCurrent)
+    }
+  },
+  watch: {
+    // 监听页码变化 -> 页码列表更新
+    page(val) {
+      if (val <= 3) {
+        this.pageList = []
+        var i = 1
+        do {
+          this.pageList.push(i)
+          i++
+        } while (i <= this.pageMax)
+        this.pageList.length > 5 && // 最多显示5页
+          (this.pageList = this.pageList.slice(0, 5))
+      } else if (val === this.pageMax) {
+        this.pageList = [val - 2, val - 1, val]
+      } else if (val === this.pageMax - 1) {
+        this.pageList = [val - 2, val - 1, val, val + 1]
+      } else {
+        this.pageList = [val - 2, val - 1, val, val + 1, val + 2]
+      }
+    },
+    // 监听页码变化 -> 总数更新
+    total(val) {
+      this.initData()
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.page {
+  padding: 10px;
+  background-color: #fff;
+
+  &-block {
+    display: inline-block;
+    width: 30px;
+    height: 28px;
+    padding: 4px 8px;
+    font-size: 0.8em;
+    line-height: 18px;
+    border: 1px solid #ddd;
+    border-right: none;
+    box-sizing: border-box;
+    cursor: pointer;
+
+    &:first-child {
+      width: 34px;
+      border-top-left-radius: 4px;
+      border-bottom-left-radius: 4px;
+    }
+
+    &:last-child {
+      width: 34px;
+      border-right: 1px solid #ddd;
+      border-top-right-radius: 4px;
+      border-bottom-right-radius: 4px;
+    }
+  }
+}
+</style>
