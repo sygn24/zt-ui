e757dde65c9a963344b1f71296e8952ecf2b0b47
路友 <1814240705@qq.com>
2022-09-14 17:13:46 +0800

    增加分页器组件，实现基本功能。 1 自定义总条数和当前页数，实现大于10页显示省略号 2 点击省略号向前或向后跳转5页 3 可选带有背景色，可选小型分页

--- examples/views/Pagination.vue
+++ examples/views/Pagination.vue
@@ -1,6 +1,42 @@
 <template>
   <div>
-    <zt-pagination :total="100" :limit="10" :page="1" @pageChange="pageChange"></zt-pagination>
+    <div class="row">
+      <h3>基础用法</h3>
+      <h4>页数少于10</h4>
+      <zt-pagination :total="60" :current-page.sync="currentPage1"></zt-pagination>
+      <h4>页数大于10</h4>
+      <zt-pagination :total="150" :current-page.sync="currentPage2"></zt-pagination>
+    </div>
+    <div class="row">
+      <h3>小型分页</h3>
+      <zt-pagination :total="60" :current-page.sync="currentPage6" small></zt-pagination>
+    </div>
+    <div class="row">
+      <h3>带有背景色</h3>
+      <zt-pagination :total="200" :current-page.sync="currentPage3" background></zt-pagination>
+    </div>
+    <div class="row">
+      <h3>自定义当前页码和每页数量，显示总条数</h3>
+      <h4>每页数量：{{pageSize}}</h4>
+      <zt-pagination
+        :total="1200"
+        :current-page.sync="currentPage4"
+        :page-size="pageSize"
+        background
+        show-total
+      ></zt-pagination>
+    </div>
+    <div class="row">
+      <h3>当前页改变的回调</h3>
+      <zt-pagination
+        :total="1200"
+        :current-page.sync="currentPage5"
+        :page-size="pageSize"
+        @current-change="pageChange"
+        background
+        show-total
+      ></zt-pagination>
+    </div>
   </div>
 </template>
 
@@ -9,15 +45,31 @@ export default {
   name: 'Pagination',
 
   data() {
-    return {}
+    return {
+      currentPage1: 1,
+      currentPage2: 1,
+      currentPage3: 1,
+      currentPage4: 10,
+      currentPage5: 8,
+      currentPage6: 2,
+      pageSize: 60
+    }
   },
   methods: {
     pageChange(page) {
-      this.$msg('第' + page + '页')
+      this.$msg('第' + this.currentPage5 + '页')
+      console.log(page)
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
+.row {
+  margin-bottom: 20px;
+  h3,
+  h4 {
+    padding: 10px;
+  }
+}
 </style>
--- packages/components/Pagination/Pagination.vue
+++ packages/components/Pagination/Pagination.vue
@@ -1,16 +1,79 @@
 <template>
-  <div class="page">
-    <span class="page-block center" @click="pageChange(1)">{{'«'}}</span>
-    <span v-if="pageList[2]-2>1" class="page-block center">...</span>
-    <span
-      v-for="pageNum in pageList"
-      :key="pageNum"
-      class="page-block center"
-      :style="{color:pageNum===page?'#80bd01':'#778087'}"
-      @click="pageChange(pageNum)"
-    >{{pageNum}}</span>
-    <span v-if="pageMax-pageList[2]>2" class="page-block center">...</span>
-    <span class="page-block center" @click="pageChange(pageMax)">{{'»'}}</span>
+  <div class="zt-pagination">
+    <!-- 总条数 -->
+    <span class="zt-pagination-total" v-show="showTotal">共 {{Math.ceil(total)}} 条</span>
+    <!-- 上一页按钮 -->
+    <button
+      type="button"
+      class="zt-pagination-prev"
+      :class="{allowed:currentPage!==1,small:small}"
+      :style="{cursor: this.currentPage === 1 ? 'not-allowed' : 'pointer',background: this.background ? '#f4f4f5' : ''}"
+      :disabled="currentPage===1"
+      @click="singleArrowClick(-1)"
+    >
+      <ZtIcon icon="arrow-left-bold" :size="small?12:13" />
+    </button>
+    <!-- 页码 -->
+    <div class="zt-pagination-pager">
+      <!-- 第一页 -->
+      <span class="zt-pagination-pager-num" :class="pageNumClass(1)" @click="staticPageClick(1)">1</span>
+      <!-- 动态页码按钮 -->
+      <div class="zt-pagination-pager-btns" v-show="!isOnePage">
+        <!-- 省略号or双箭头left -->
+        <span
+          title="向前5页"
+          class="zt-pagination-pager-num"
+          :class="{small:small}"
+          :style="background?'background:#f4f4f5':''"
+          v-show="!isLessThan10 && doublePrev"
+          @mouseenter="ellipsisPrev=false"
+          @mouseleave="ellipsisPrev=true"
+          @click="doubleArrowClick(-1)"
+        >
+          <ZtIcon :icon="ellipsisPrev?'ellipsis':'double-arrow-left'" />
+        </span>
+        <!-- 动态页码 -->
+        <span
+          v-for="page in dynamicPageArr"
+          :key="page"
+          class="zt-pagination-pager-num"
+          :class="pageNumClass(page)"
+          @click="dynamicPageClick(page)"
+        >{{page}}</span>
+        <!-- 省略号or双箭头right -->
+        <span
+          title="向后5页"
+          class="zt-pagination-pager-num"
+          :class="{small:small}"
+          :style="background?'background:#f4f4f5':''"
+          v-show="!isLessThan10 && doubleNext"
+          @mouseenter="ellipsisNext=false"
+          @mouseleave="ellipsisNext=true"
+          @click="doubleArrowClick(1)"
+        >
+          <ZtIcon :icon="ellipsisNext?'ellipsis':'double-arrow-right'" />
+        </span>
+      </div>
+      <!-- 最后一页 -->
+      <span
+        class="zt-pagination-pager-num"
+        :class="pageNumClass(lastPageNum)"
+        style="margin-right:0"
+        v-show="!isOnePage"
+        @click="staticPageClick(lastPageNum)"
+      >{{lastPageNum}}</span>
+    </div>
+    <!-- 下一页按钮 -->
+    <button
+      type="button"
+      class="zt-pagination-next"
+      :class="{allowed:currentPage!==lastPageNum,small:small}"
+      :style="{cursor: this.currentPage === lastPageNum ? 'not-allowed' : 'pointer',background: this.background ? '#f4f4f5' : ''}"
+      :disabled="currentPage===lastPageNum"
+      @click="singleArrowClick(1)"
+    >
+      <ZtIcon icon="arrow-right-bold" :size="small?12:13" />
+    </button>
   </div>
 </template>
 
@@ -18,109 +81,175 @@
 export default {
   name: 'ZtPagination',
   props: {
-    // 内容总数
+    // 总条数
     total: {
       type: Number,
-      default: 0
+      require: true
     },
-    // 每页数量
-    limit: {
+    // 当前页
+    currentPage: {
+      type: Number,
+      default: 1
+    },
+    // 每页显示数量
+    pageSize: {
       type: Number,
       default: 10
     },
-    // 当前页码
-    page: {
-      type: Number,
-      default: 1
+    // 是否为分页按钮添加背景色
+    background: {
+      type: Boolean,
+      default: false
+    },
+    // 是否显示总条数
+    showTotal: {
+      type: Boolean,
+      default: false
+    },
+    // 是否开启小型分页样式
+    small: {
+      type: Boolean,
+      default: false
     }
   },
+
   data() {
     return {
-      pageList: [] // 页码列表
+      doublePrev: false, //是否显示左双箭头
+      doubleNext: false, //是否显示右双箭头
+      ellipsisPrev: true, //省略号or左双箭头
+      ellipsisNext: true, //省略号or右双箭头
+      dynamicPageArr: [] //动态页码数组
     }
+  },
+  mounted() {
+    this.initDynamicPage()
+    this.initCurrent()
   },
   computed: {
-    // 最大页数
-    pageMax() {
-      return Math.ceil(this.total / this.limit)
+    // 最后一页页码
+    lastPageNum() {
+      if (this.total <= 0) return 1
+      return Math.ceil(this.total / this.pageSize)
+    },
+    // 是否为一页
+    isOnePage() {
+      if (this.lastPageNum > 1) return false
+      return true
+    },
+    // 是否小于10页
+    isLessThan10() {
+      if (this.lastPageNum < 10) return true
+      return false
     }
   },
-  created() {
-    this.initData()
-  },
   methods: {
-    initData() {
-      this.pageList = [] // 清空页码
-      var i = 1
-      do {
-        this.pageList.push(i)
-        i++
-      } while (i <= this.pageMax)
-      this.pageList.length > 5 && // 最多显示5页
-        (this.pageList = this.pageList.slice(0, 5))
+    // 初始化动态页码
+    initDynamicPage() {
+      let arrLength = this.isLessThan10 ? this.lastPageNum - 2 : 5
+      this.dynamicPageArr = Array.from(
+        { length: arrLength },
+        (_, index) => index + 2
+      )
+      // 如果大于10页，根据当前页设置动态页码
+      !this.isLessThan10 && this.setDynamicPage(this.currentPage)
     },
-    // 子组件事件回调：分页
-    pageChange(pageCurrent) {
-      this.$emit('pageChange', pageCurrent)
+    // 初始化当前页码
+    initCurrent() {
+      if (this.currentPage > this.lastPageNum) {
+        this.$emit('update:currentPage', this.lastPageNum)
+      }
+      if (this.currentPage < 1) {
+        this.$emit('update:currentPage', 1)
+      }
+    },
+    //根据当前页码设置动态页码
+    setDynamicPage(currentPage) {
+      let pageStart = currentPage - 2
+      pageStart = pageStart < 2 ? 2 : pageStart
+      pageStart =
+        pageStart > this.lastPageNum - 5 ? this.lastPageNum - 5 : pageStart
+      this.dynamicPageArr = Array.from(
+        { length: 5 },
+        (_, index) => index + pageStart
+      )
+      // 设置是否显示省略号双箭头
+      let lastIndex = this.dynamicPageArr.length - 1
+      this.doublePrev = this.dynamicPageArr[0] === 2 ? false : true
+      this.doubleNext =
+        this.dynamicPageArr[lastIndex] === this.lastPageNum - 1 ? false : true
+    },
+    // 更新当前页码
+    updateCurrentPage(currentPage) {
+      this.$emit('update:currentPage', currentPage)
+      this.$emit('current-change', currentPage)
+    },
+    //静态页码点击，即点击第一页或最后一页
+    staticPageClick(currentPage) {
+      if (currentPage === this.currentPage) return
+      this.updateCurrentPage(currentPage)
+      if (this.doublePrev || this.doubleNext) {
+        this.setDynamicPage(currentPage)
+      }
+    },
+    // 动态页码点击
+    dynamicPageClick(currentPage) {
+      if (currentPage === this.currentPage) return
+      this.updateCurrentPage(currentPage)
+      // 如果显示向右的双箭头并且点击的是动态页码最后两项，重新设置动态页码
+      if (this.doubleNext) {
+        let lastIndex = this.dynamicPageArr.length - 1
+        if (
+          currentPage === this.dynamicPageArr[lastIndex] ||
+          currentPage === this.dynamicPageArr[lastIndex - 1]
+        ) {
+          this.setDynamicPage(currentPage)
+        }
+      }
+      //如果显示向左的双箭头并且点击的是动态页码前两项，重新设置动态页码
+      if (this.doublePrev) {
+        if (
+          currentPage === this.dynamicPageArr[0] ||
+          currentPage === this.dynamicPageArr[1]
+        ) {
+          this.setDynamicPage(currentPage)
+        }
+      }
+    },
+    //点击省略号双箭头
+    doubleArrowClick(num) {
+      let current = num === 1 ? this.currentPage + 5 : this.currentPage - 5
+      current = current > 0 ? current : 1
+      current = current < this.lastPageNum ? current : this.lastPageNum
+      this.setDynamicPage(current)
+      this.updateCurrentPage(current)
+    },
+    // 点击单箭头上一页or下一页
+    singleArrowClick(num) {
+      let current = num === 1 ? this.currentPage + 1 : this.currentPage - 1
+      this.dynamicPageClick(current)
+    },
+    // 动态设置页码类名
+    pageNumClass(page) {
+      return {
+        bg: this.background,
+        active: !this.background && page === this.currentPage,
+        'active-bg': this.background && page === this.currentPage,
+        small: this.small
+      }
     }
   },
   watch: {
-    // 监听页码变化 -> 页码列表更新
-    page(val) {
-      if (val <= 3) {
-        this.pageList = []
-        var i = 1
-        do {
-          this.pageList.push(i)
-          i++
-        } while (i <= this.pageMax)
-        this.pageList.length > 5 && // 最多显示5页
-          (this.pageList = this.pageList.slice(0, 5))
-      } else if (val === this.pageMax) {
-        this.pageList = [val - 2, val - 1, val]
-      } else if (val === this.pageMax - 1) {
-        this.pageList = [val - 2, val - 1, val, val + 1]
-      } else {
-        this.pageList = [val - 2, val - 1, val, val + 1, val + 2]
-      }
+    total() {
+      this.initDynamicPage()
     },
-    // 监听页码变化 -> 总数更新
-    total(val) {
-      this.initData()
+    pageSize() {
+      this.initDynamicPage()
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-.page {
-  padding: 10px;
-  background-color: #fff;
-
-  &-block {
-    display: inline-block;
-    width: 30px;
-    height: 28px;
-    padding: 4px 8px;
-    font-size: 0.8em;
-    line-height: 18px;
-    border: 1px solid #ddd;
-    border-right: none;
-    box-sizing: border-box;
-    cursor: pointer;
-
-    &:first-child {
-      width: 34px;
-      border-top-left-radius: 4px;
-      border-bottom-left-radius: 4px;
-    }
-
-    &:last-child {
-      width: 34px;
-      border-right: 1px solid #ddd;
-      border-top-right-radius: 4px;
-      border-bottom-right-radius: 4px;
-    }
-  }
-}
+@import './style.less';
 </style>
