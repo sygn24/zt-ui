5b7937140988f9e8c4970fed19df71b5122a96c2
路友 <1814240705@qq.com>
2022-09-20 17:36:30 +0800

    update

--- examples/views/Icon.vue
+++ examples/views/Icon.vue
@@ -16,6 +16,7 @@ export default {
     data() {
         return {
             iconList: [
+                'search',
                 'clear',
                 'ellipsis',
                 'double-arrow-right',
