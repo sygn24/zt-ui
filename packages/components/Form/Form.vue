460b54988a9b4154019eae35698f85bb35e3b962
路友 <1814240705@qq.com>
2022-10-28 09:01:43 +0800

    更新Form组件

--- packages/components/Form/Form.vue
+++ packages/components/Form/Form.vue
@@ -50,7 +50,3 @@ export default {
     }
 }
 </script>
-
-<style lang="less" scoped>
-@import './style.less';
-</style>
--- packages/components/Form/index.js
+++ packages/components/Form/index.js
@@ -1,3 +1,12 @@
 import Form from './Form.vue'
 import FormItem from './FormItem.vue'
+
+Form.install = function (Vue) {
+    Vue.component(Form.name, Form)
+}
+
+FormItem.install = function (Vue) {
+    Vue.component(FormItem.name, FormItem)
+}
+
 export { Form, FormItem }
